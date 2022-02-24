388febd4cbca7705363878283dc8a78bb9811dfc
Bruce Wright <bruce.wright@metoffice.gov.uk>
2022-02-24 16:42:08 +0000

    conf.py updated to use sphinx_rtd_theme html theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -74,6 +74,7 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.mathjax",
     "sphinx_autodoc_typehints",
+    "sphinx_rtd_theme",
 ]
 
 autodoc_default_flags = ["members", "private-members"]
@@ -168,7 +169,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "default"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
